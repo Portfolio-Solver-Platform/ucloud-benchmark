7cc6c4720d135b33102da1320aa70949a55db8cc
sofus <sofus@laubel.net>
2026-04-16 10:29:04 +0200

    sync

---
+++ run-eligible-portfolios/run-k2-7c-8s-v1-p1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+cd "$(dirname "$0")"
+
+PORTFOLIO="k2-7c-8s-v1-p1"
+CORES=7
+
+for year in 2025 2024 2023; do
+    echo "=== ${PORTFOLIO} ${year} ==="
+    python ../benchmark_parasol.py -s "../schedules-eligible/${PORTFOLIO}.csv" \
+        -r 1 -t 1200 \
+        -o "../../results/portfolios-eligible/${PORTFOLIO}/${PORTFOLIO}-${year}" \
+        --problems-path "../../data/mznc${year}_probs" \
+        --discover \
+        -- --solver parasol -p ${CORES} --ai none --output-solver \
+        --solver-config-mode cache --verbosity error \
+        --static-runtime 100000000
+done 2>&1 | tee ${PORTFOLIO}-out.txt
