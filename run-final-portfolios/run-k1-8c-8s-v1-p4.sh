978c7873df1a176d6e0737a1db322463f108814d
sofus <sofus@laubel.net>
2026-04-20 15:22:02 +0200

    sync

---
+++ run-final-portfolios/run-k1-8c-8s-v1-p4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+cd "$(dirname "$0")"
+
+PORTFOLIO="k1-8c-8s-v1"
+CORES=8
+
+for year in 2020 2021 2022; do
+    echo "=== ${PORTFOLIO} ${year} ==="
+    python ../benchmark_parasol.py -s "../schedules/k1-8c-8s-v1.csv" \
+        -r 1 -t 1200 \
+        -o "../../results/portfolios-final/${PORTFOLIO}/${PORTFOLIO}-${year}" \
+        --problems-path "../../data/mzn-challenge/${year}" \
+        --discover \
+        -- --solver parasol -p ${CORES} --ai none --output-solver \
+        --solver-config-mode cache --verbosity error \
+        --static-runtime 100000000
+done 2>&1 | tee ${PORTFOLIO}-p4-out.txt
