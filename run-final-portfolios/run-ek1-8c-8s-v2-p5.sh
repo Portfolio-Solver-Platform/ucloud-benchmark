174e8f70c4b674c85c39cbcc3268ec2ad46dbc67
sofus <sofus@laubel.net>
2026-04-23 15:59:51 +0200

    sync

---
+++ run-final-portfolios/run-ek1-8c-8s-v2-p5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+cd "$(dirname "$0")"
+
+PORTFOLIO="ek1-8c-8s-v2"
+CORES=8
+
+for year in 2023 2024 2025; do
+    echo "=== ${PORTFOLIO} ${year} ==="
+    python ../benchmark_parasol.py -s "../schedules-eligible/${PORTFOLIO}.csv" \
+        -r 1 -t 1200 \
+        -o "../../results/portfolios-final/${PORTFOLIO}/${PORTFOLIO}-${year}" \
+        --problems-path "../../data/mzn-challenge/${year}" \
+        --discover \
+        -- --solver parasol -p ${CORES} --ai none --output-solver \
+        --solver-config-mode cache --verbosity error \
+        --static-runtime 100000000
+done 2>&1 | tee ${PORTFOLIO}-p5-out.txt
