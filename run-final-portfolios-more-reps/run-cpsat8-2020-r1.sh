397dfb80856341811c159f7c4bdae8f70b6f8a5b
sofus <sofus@laubel.net>
2026-05-05 15:04:36 +0200

    sync

---
+++ run-final-portfolios-more-reps/run-cpsat8-2020-r1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eo pipefail
+cd "$(dirname "$0")"
+
+PORTFOLIO="cpsat8"
+CORES=8
+year=2020
+rep=1
+
+{
+    echo "=== ${PORTFOLIO} ${year} r${rep} ==="
+    python ../benchmark_parasol.py -s "../solvers/cpsat8.csv" \
+        -r 1 -t 1200 \
+        -o "../../results/final-many-reps/${PORTFOLIO}/${PORTFOLIO}-${year}-r${rep}" \
+        --problems-path "../../data/mzn-challenge/${year}" \
+        --discover \
+        -- --solver parasol -p ${CORES} --ai none --output-solver \
+        --solver-config-mode cache --verbosity error \
+        --static-runtime 100000000
+} 2>&1 | tee ${PORTFOLIO}-${year}-r${rep}-out.txt
